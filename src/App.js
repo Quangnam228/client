5f017df5ea61cd76582a5bd496a8876366fc5aa0
QuangNam <quangnam228237@gmail.com>
2022-01-11 13:09:31 +0700

    "add client"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Auth from "./components/auth/Auth";
+import Landing from "./components/Layout/Landing";
+import Dashboard from "./components/dashboard/Dashboard";
+import LoginForm from "./components/auth/LoginForm";
+import RegisterForm from "./components/auth/RegisterForm";
+import ProtectedRoute from "./components/routing/ProtectedRoute";
+import PostContextProvider from "./contexts/PostContext";
+import AuthContextProvider from "./contexts/AuthContext";
+import About from "./view/About";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <PostContextProvider>
+          <Routes>
+            <Route element={<Landing />} path="/" />
+            <Route path="/" element={<Auth />}>
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterForm />} />
+            </Route>
+            <Route path="/" element={<ProtectedRoute />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </PostContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
